f947b6817aef1a4363c8a82b7264953f53b9913f
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-11 11:58:22 +0530

    swap without temp

---
+++ Dsa-with-java/src/GeneralProblem/SwapStrelementWithoutTemp.java
@@ -0,0 +1,20 @@
+package GeneralProblem;
+
+public class SwapStrelementWithoutTemp {
+
+	public static void main(String[] args) {
+
+		String str = "abc";
+		String str1 = "efgh";
+
+		str = str + str1;
+		System.out.println(str);
+		str1 = str.substring(0, str.length() - str1.length());
+		str = str.substring(str1.length());
+
+		System.out.println("str " + str);
+		System.out.println("str1 " + str1);
+
+	}
+
+}
