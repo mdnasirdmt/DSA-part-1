9964138be170424690f13a58643bcae194013ea7
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-11 15:44:33 +0530

    prime

--- Dsa-with-java/src/GeneralProblem/Prime.java
+++ Dsa-with-java/src/GeneralProblem/Prime.java
@@ -2,13 +2,13 @@ package GeneralProblem;
 
 public class Prime {
 
-	
 	public static void main(String[] args) {
-		
-		
-		int x=19;
-		
-		if(x%2==0)
+
+		int x = 2;
+
+		if (x == 2)
+			System.out.println("prime");
+		else if (x % 2 == 0)
 			System.out.println("not prime");
 		else
 			System.out.println("prime ");
