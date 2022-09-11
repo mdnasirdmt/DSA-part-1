95c5d486701ce74a8a34dabd5a6973ef9327cf91
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-11 11:12:48 +0530

    integer palindrom

---
+++ Dsa-with-java/src/GeneralProblem/PalindromOrNotInteger.java
@@ -0,0 +1,45 @@
+package GeneralProblem;
+
+public class PalindromOrNotInteger {
+
+	public static void main(String[] args) {
+		
+		int n=1221;
+		
+		palindrom(n);
+		
+	}
+
+	private static void palindrom(int n) {
+		
+		int n1=n, rem=0, num=0;
+		
+		while(n>0) {
+			
+			rem= n%10;
+			num=num*10+rem;
+			n=n/10;
+		}
+		
+		if(n1==num) {
+			System.out.println("yes");
+		}else {
+			System.out.println("no");
+		}
+	}
+
+//	private static void palindrom(int n) {
+//		
+//		String s2= String.valueOf(n);
+//		String s1=new StringBuilder(s2).reverse().toString();
+//		
+//		
+//		if(s1.equals(s2)) {
+//			
+//			System.out.println("yes");
+//		}else {
+//			System.out.println("no");
+//		}
+//		
+//	}
+}
