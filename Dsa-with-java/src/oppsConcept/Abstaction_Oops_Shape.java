baa8507709105fc8ec39af3eb3cee78df0d5927f
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-13 07:56:16 +0530

    abstarction in oops

--- Dsa-with-java/src/oppsConcept/Abstaction_Oops_Shape.java
+++ Dsa-with-java/src/oppsConcept/Abstaction_Oops_Shape.java
@@ -83,3 +83,21 @@ class Rectangle extends Shape {
 }
 
 //////
+
+/*
+ * 
+ * Abstract classes and Abstract methods :
+ * 
+ * An abstract class is a class that is declared with an abstract keyword. An
+ * abstract method is a method that is declared without implementation. An
+ * abstract class may or may not have all abstract methods. Some of them can be
+ * concrete methods A method-defined abstract must always be redefined in the
+ * subclass, thus making overriding compulsory or making the subclass itself
+ * abstract. Any class that contains one or more abstract methods must also be
+ * declared with an abstract keyword. There can be no object of an abstract
+ * class. That is, an abstract class can not be directly instantiated with the
+ * new operator. An abstract class can have parameterized constructors and the
+ * default constructor is always present in an abstract class.
+ * 
+ * 
+ */
