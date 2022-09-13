a3116f7bee76a1e754145b0f5398a4939a9c7ff6
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-14 00:42:25 +0530

    super class

--- Dsa-with-java/src/oppsConcept/Abstaction_Oops_Shape.java
+++ Dsa-with-java/src/oppsConcept/Abstaction_Oops_Shape.java
@@ -4,10 +4,9 @@ public class Abstaction_Oops_Shape {
 
 	public static void main(String[] args) {
 		Shape s1 = new Circle("Red", 2.2);
-		Shape s2 = new Rectangle("Yellow", 2, 4);
 
 		System.out.println(s1.toString());
-		System.out.println(s2.toString());
+
 	}
 }
 
