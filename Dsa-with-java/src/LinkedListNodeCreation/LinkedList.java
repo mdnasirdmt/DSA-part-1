3a133c2ea481cdc8dfcdd1facf68ffe5360b41f6
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-23 08:46:36 +0530

    Node creation

---
+++ Dsa-with-java/src/LinkedListNodeCreation/LinkedList.java
@@ -0,0 +1,19 @@
+package LinkedListNodeCreation;
+
+public class LinkedList {
+	Node head;
+		
+	class Node{
+		
+		int data;
+		Node next;
+		
+		Node(int data){
+			this.data=data;
+			next=null;
+		}
+	}
+
+}
+
+
