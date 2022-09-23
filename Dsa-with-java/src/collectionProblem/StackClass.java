5e8b08d7ae6a95f56824eb177c1c211c955071de
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-09-23 08:27:22 +0530

    stack class

---
+++ Dsa-with-java/src/collectionProblem/StackClass.java
@@ -0,0 +1,50 @@
+package collectionProblem;
+
+import java.util.*;
+public class StackClass {
+
+	public static void main(String[] args) {
+		
+		List<Integer> s= new Stack<>();
+		Stack<Integer> s1= new Stack<>();
+		s1.add(null);
+		s.add(null);
+		
+		for(int i=0; i<10;i++) {
+			s.add(i);
+			s1.add(i);
+		}
+		
+		s.add(20);
+//		s.remove(1);
+		
+		((Stack<Integer>) s).push(12);
+		s1.push(13);
+		
+		((Stack<Integer>) s).pop();
+		s1.pop();
+		
+		System.out.println(((Stack<Integer>) s).peek());
+		System.out.println(s1.peek());
+		
+		System.out.println(s1.search(8));
+		
+		s1.addAll(0,s);
+		s1.addAll(s);
+		
+		
+		System.out.println("toArray "+Arrays.toString(s.toArray()));
+		System.out.println("toWString "+ s.toString());
+		System.out.println("aslist "+Arrays.asList(s1));
+		s1.trimToSize();
+		System.out.println("trim s1"+s1);
+		
+		System.out.println(((Vector<Integer>) s).capacity());
+		
+		
+		System.out.println("stack "+s);
+		System.out.println("stack1 "+s1);
+		
+		
+	}
+}
