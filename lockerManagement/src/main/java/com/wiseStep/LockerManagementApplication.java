5754a369079ff4584deb99fb3a9768d025a0a467
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-10-11 06:39:02 +0530

    payment wallet app created

---
+++ lockerManagement/src/main/java/com/wiseStep/LockerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.wiseStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerManagementApplication.class, args);
+	}
+
+}
---
+++ lockerManagement/src/test/java/com/wiseStep/LockerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiseStep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
