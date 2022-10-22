501510302d3cfddff977c59fc10c308b13094e36
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-10-22 10:23:11 +0530

    multi threading using thread class

--- Dsa-with-java/src/test/MultiThreading.java
+++ Dsa-with-java/src/test/MultiThreading.java
@@ -1,29 +1,27 @@
 package test;
 
-public class MultiThreading implements Runnable{
+public class MultiThreading implements Runnable {
 
 	@Override
 	public void run() {
-		
-		for(int i=0;i<3;i++)
-			System.out.println("custome thread"+i);
-		
+
+		for (int i = 0; i < 3; i++)
+			System.out.println("custome thread" + i);
+
 	}
 
-	
 	public static void main(String[] args) {
-		
-		MultiThreading m= new MultiThreading();
-		
-		Thread t1= new Thread(m);
-		
-		
+
+		MultiThreading m = new MultiThreading();
+
+		Thread t1 = new Thread(m);
+
 //		m.run();// it will act as normal method
 //		t1.run();// it will also act as normal method
 		t1.start();// it will act as thread
-		
-		for(int i=0;i<3;i++)
-			System.out.println("main thread"+i);
-		
+
+		for (int i = 0; i < 3; i++)
+			System.out.println("main thread" + i);
+
 	}
 }
--- Dsa-with-java/src/test/MultiThreadingByThreadClass.java
+++ Dsa-with-java/src/test/MultiThreadingByThreadClass.java
@@ -1,25 +1,25 @@
 package test;
 
 public class MultiThreadingByThreadClass {
-	
+
 	public static void main(String[] args) {
-		
-		ThreadClass t1= new ThreadClass();
-		
-		t1.start();
-		
-		for(int i=0;i<3;i++)
-			System.out.println("hello main thread "+i);
+
+		ThreadClass t1 = new ThreadClass();
+
+		t1.start();// custome tread created
+
+		for (int i = 0; i < 3; i++)
+			System.out.println("hello main thread " + i);
 	}
 
 }
 
-class ThreadClass extends Thread{
-	
+class ThreadClass extends Thread {
+
 	@Override
 	public void run() {
-		for(int i=0;i<3;i++)
-			System.out.println("hello thread "+i);
+		for (int i = 0; i < 3; i++)
+			System.out.println("hello thread " + i);
 	}
-	
+
 }
