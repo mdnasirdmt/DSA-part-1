1ae64040be2bbc6663c07ef1625e5fb85a352296
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-11-26 09:30:44 +0530

    even or odd without modulus

--- dsa-part-2/src/generalProblems/EvenWithoutModulus.java
+++ dsa-part-2/src/generalProblems/EvenWithoutModulus.java
@@ -4,16 +4,41 @@ public class EvenWithoutModulus {
 
 	public static void main(String[] args) {
 		
-		int x=10;
-		int i= x&1;
+		int x=11;
 		
-		System.out.println(i);
-		if(i == 0) {
+		// 1st method
+//		int i= x&1;
+//		
+//		System.out.println(i);
+//		if(i == 0) {
+//			System.out.println("even");
+//		}else {
+//			System.out.println("odd");
+//		}
+		/////
+		
+		
+		
+		
+//		2nd method
+//		boolean f=true;
+//		for(int i=0;i<x;i++) {
+//			f=!f;
+//			System.out.println(f);
+//		}
+//		if(f)
+//			System.out.println("even");
+//		else
+//			System.out.println("odd");
+		
+///
+		
+//		3rd method
+		if((x/2)*2==x){
 			System.out.println("even");
 		}else {
 			System.out.println("odd");
 		}
-
 	}
 
 }
