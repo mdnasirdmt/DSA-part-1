d940cc66e8b45b13d31b43a83e67eca7d65461f6
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-11-30 08:08:36 +0530

    perfect num

--- dsa-part-2/src/generalProblems/PerfectNum.java
+++ dsa-part-2/src/generalProblems/PerfectNum.java
@@ -5,12 +5,27 @@ import java.util.Scanner;
 public class PerfectNum {
 
 	public static void main(String[] args) {
-		
-		Scanner s= new Scanner(System.in);
-		System.out.println("eneter number ");
-		int n= s.nextInt();
-		
 
+		Scanner s = new Scanner(System.in);
+		System.out.println("eneter number ");
+		int n = s.nextInt();
+
+		int sum = 0;
+		for (int i = 1; i < n; i++)
+			if (n % i == 0)
+				sum += i;
+		if (sum == n)
+			System.out.println(n + " is perfect number " + sum);
+		else
+			System.out.println(n + " is not perfecr number ");
+
+		System.out.println(n + " " + sum);
 	}
 
 }
+
+/*
+ * Write program weather the number is PERFECT NUMBER or not? Def: Perfect
+ * number, a positive integer that is equal to the sum of its proper divisors.
+ * The smallest perfect number is 6, which is the sum of 1, 2, and 3.
+ */
