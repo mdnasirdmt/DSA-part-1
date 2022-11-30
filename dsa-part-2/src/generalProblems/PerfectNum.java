faa92f2890afb4f54296b30166e9b4cabc2c6775
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-11-30 08:12:16 +0530

    perfect num

--- dsa-part-2/src/generalProblems/PerfectNum.java
+++ dsa-part-2/src/generalProblems/PerfectNum.java
@@ -10,8 +10,8 @@ public class PerfectNum {
 		System.out.println("eneter number ");
 		int n = s.nextInt();
 
-		int sum = 0;
-		for (int i = 1; i < n; i++)
+		int sum = 1;
+		for (int i = 2; i <=n/2; i++)
 			if (n % i == 0)
 				sum += i;
 		if (sum == n)
