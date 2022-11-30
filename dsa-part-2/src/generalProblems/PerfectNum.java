9ce8a6436e7bd311d2cb997b968595f49b085628
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-11-30 07:54:30 +0530

    perfect num

---
+++ dsa-part-2/src/generalProblems/PerfectNum.java
@@ -0,0 +1,16 @@
+package generalProblems;
+
+import java.util.Scanner;
+
+public class PerfectNum {
+
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		System.out.println("eneter number ");
+		int n= s.nextInt();
+		
+
+	}
+
+}
