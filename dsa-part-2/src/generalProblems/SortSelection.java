a7bc4285234cf261aaa3d3dec3cb17dc11da76f7
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-01 08:30:03 +0530

    selection sort

---
+++ dsa-part-2/src/generalProblems/SortSelection.java
@@ -0,0 +1,30 @@
+package generalProblems;
+
+import java.util.Arrays;
+
+public class SortSelection {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 3, 1, 2, 4, 8, 3, 6, 7, 0 };
+
+		int n = arr.length;
+
+		for (int i = 0; i < n - 1; i++) {
+			int min = i;
+
+			for (int j = i + 1; j < n; j++) {
+				if (arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			int t = arr[min];
+			arr[min] = arr[i];
+			arr[i] = t;
+		}
+
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
