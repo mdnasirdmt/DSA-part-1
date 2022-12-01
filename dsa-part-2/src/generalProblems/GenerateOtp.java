2e46841de6f51278d00553de95e57c66bcfb01f2
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-01 08:47:41 +0530

    generate otp

--- dsa-part-2/src/generalProblems/GenerateOtp.java
+++ dsa-part-2/src/generalProblems/GenerateOtp.java
@@ -3,9 +3,9 @@ package generalProblems;
 public class GenerateOtp {
 
 	public static void main(String[] args) {
-		
-		int otp=(int) (Math.random()*9000)+1000;
-		
+
+		int otp = (int) (Math.random() * 9000) + 1000;
+
 		System.out.println(otp);
 
 	}
