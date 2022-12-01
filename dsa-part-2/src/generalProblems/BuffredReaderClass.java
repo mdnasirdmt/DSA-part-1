c850f16e527bf6da75d9b7fc497c6314446acaf5
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-01 18:12:42 +0530

    file creation

--- dsa-part-2/src/generalProblems/BuffredReaderClass.java
+++ dsa-part-2/src/generalProblems/BuffredReaderClass.java
@@ -7,13 +7,13 @@ import java.io.InputStreamReader;
 public class BuffredReaderClass {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		System.out.println("enter value ");
-		
-		int n= Integer.parseInt(br.readLine());
-		System.out.println("num is "+ n);
+
+		int n = Integer.parseInt(br.readLine());
+		System.out.println("num is " + n);
 
 	}
 
