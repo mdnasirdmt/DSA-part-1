bdffbed4e68bd3587a83c98ed09e83902c55dd7c
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-01 18:21:52 +0530

    finally try catch block

--- Dsa-with-java/src/test/FinallyBlock.java
+++ Dsa-with-java/src/test/FinallyBlock.java
@@ -4,8 +4,8 @@ public class FinallyBlock {
 	public static void main(String[] args) {
 		try {
 			System.out.println("I am in try block");
-			System.exit(2);
-//			return;
+			System.exit(0);
+			return;
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		} finally {
