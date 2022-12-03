4c9d4ab7005b9fc10bea9ccb681c290f8cd63ea7
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-03 17:55:54 +0530

    spring boot basic application

--- Hello/src/main/java/com/masai/controller/ControllerClass.java
+++ Hello/src/main/java/com/masai/controller/ControllerClass.java
@@ -1,6 +1,7 @@
 package com.masai.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ControllerClass {
 
-//	@GetMapping(value = "/")
-	@GetMapping()
-	public String printHandler() {
+//	@GetMapping()
+//	public String printHandler() {
+//
+//		return "spring boot ki duniya me apka swagat hai!";
+//
+//	}
+	
+	
+//	@GetMapping(value = "h")
+//	public String printHandler() {
+//
+//		return "spring boot ki duniya me apka swagat hai!";
+//
+//	}
+	
+
+	@GetMapping(value = "{id}")
+	public String printHandler(@PathVariable Integer id) {
 
 		return "spring boot ki duniya me apka swagat hai!";
 
