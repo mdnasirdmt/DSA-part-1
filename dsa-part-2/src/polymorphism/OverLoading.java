23f886e34f9964fb26c1f14862356563143f3c60
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-03 09:17:44 +0530

    overloading

---
+++ dsa-part-2/src/polymorphism/OverLoading.java
@@ -0,0 +1,25 @@
+package polymorphism;
+
+public class OverLoading {
+	
+	void methode(String str) {
+		System.out.println("methode "+ str);
+	}
+	
+	static int methode(int a,int b){
+		return a+b;
+	} 
+	
+	
+
+	public static void main(String[] args) {
+		
+		OverLoading ol= new OverLoading();
+		ol.methode(1,2);
+		ol.methode("welcome");
+
+	}
+	
+	
+
+}
