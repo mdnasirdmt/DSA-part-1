ae89be13fb8908bc3b773f6a231150335f72a185
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-13 17:19:45 +0530

    remove duplicate  element

--- dsa-part-2/src/generalProblems/RemoveDuplicateFromArray.java
+++ dsa-part-2/src/generalProblems/RemoveDuplicateFromArray.java
@@ -3,39 +3,36 @@ package generalProblems;
 public class RemoveDuplicateFromArray {
 
 	public static void main(String[] args) {
-		
 
-		int[]arr= {2,1,3,3,5,6,2,3,7};
-		int n=arr.length;
-		
+		int[] arr = { 2, 1, 3, 3, 5, 6, 2, 3, 7 };
+		int n = arr.length;
+
 		// maximum in array
-		int m=0;
+		int m = 0;
 		for (int i = 0; i < arr.length; i++) {
-			m= Math.max(m, arr[i]);
+			m = Math.max(m, arr[i]);
 		}
-		
+
 //		System.out.println(m);
-		
-		
+
 		// to find number of occurance of element in arr
-		int[] freq= new int[m+1];
-		
+		int[] freq = new int[m + 1];
+
 		for (int i = 0; i < arr.length; i++) {
-			
+
 			freq[arr[i]]++;
 
 //			System.out.println(freq[arr[i]]);
 		}
-		
+
 		for (int i = 0; i < freq.length; i++) {
-			
-			if(freq[i]>=1) {
-				System.out.print(i+" ");
+
+			if (freq[i] >= 1) {
+				System.out.print(i + " ");
 			}
-			
+
 		}
-		
-		
+
 	}
 
 }
