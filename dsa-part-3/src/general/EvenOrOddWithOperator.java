12194579cc733ffc442a32066d7f3d1570e147cc
Md Nasir uddin <mdnasirdmt@gmail.com>
2022-12-15 08:53:16 +0530

    even or not

--- dsa-part-3/src/general/EvenOrOddWithOperator.java
+++ dsa-part-3/src/general/EvenOrOddWithOperator.java
@@ -6,6 +6,7 @@ public class EvenOrOddWithOperator {
 
 		int x = 81;
 
+		// with logical operator
 		int n = x & 1;
 
 		if (n == 0)
@@ -13,6 +14,11 @@ public class EvenOrOddWithOperator {
 		else
 			System.out.println("odd - " + n);
 
+		/// with modulus operator
+		if (x % 2 == 0)
+			System.out.println("even ");
+		else
+			System.out.println("odd ");
 	}
 
 }
