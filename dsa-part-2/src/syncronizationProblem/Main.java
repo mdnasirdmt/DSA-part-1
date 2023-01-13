53ad25433d4e65ec22d11b8f939bca08e86110a5
Md Nasir Uddin <mdnasirdmt@gmail.com>
2023-01-13 09:05:00 +0530

    syncronization concept completed

--- dsa-part-2/src/syncronizationProblem/Main.java
+++ dsa-part-2/src/syncronizationProblem/Main.java
@@ -1,16 +1,33 @@
 package syncronizationProblem;
 
 public class Main {
-	public static void main(String[] args) {
 
-		Syncro1 syncro1 = new Syncro1();
-		Syncro2 syncro2 = new Syncro2();
+	synchronized void printValue(int n) {
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.println(i * n);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Syncro1 syncro1 = new Syncro1(new Main());
+		Syncro2 syncro2 = new Syncro2(new Main());
 
 		Thread t1 = new Thread(syncro1);
 		Thread t2 = new Thread(syncro2);
 
-		t1.start();
 		t2.start();
+		t2.join();
+		
+		t1.start();
+		t1.join();
 
 	}
 
@@ -18,38 +35,31 @@ public class Main {
 
 class Syncro1 implements Runnable {
 
+	Main m;
+
+	public Syncro1(Main m) {
+		this.m = m;
+	}
+
 	@Override
 	public void run() {
-
-		for (int i = 1; i <= 3; i++) {
-			System.out.println(i * 100);
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
+		m.printValue(500);
 	}
 
 }
 
 class Syncro2 implements Runnable {
+	
+	Main m;
+
+	public Syncro2(Main m) {
+		this.m = m;
+	}
 
 	@Override
 	public void run() {
-		for (int i = 1; i <= 3; i++) {
-			System.out.println(i * 500);
-			
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
+		
+		m.printValue(300);
 	}
 
 }
