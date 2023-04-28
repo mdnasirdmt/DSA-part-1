3b729865bedfd6144a1c7276126e4daf0d346e13
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-04-28 17:40:14 +0530

    two pointers

--- dsa-part-3/src/twoPointers/Pairs.java
+++ dsa-part-3/src/twoPointers/Pairs.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
 public class Pairs {
 
 	public static void main(String[] args) {
-		
-		int[]arr= {3,4,0,2,7};
-		int k=2;
+
+		int[] arr = { 3, 4, 0, 2, 7 };
+		int k = 2;
 		Arrays.sort(arr);
-		System.out.println(pairs( arr, arr.length, k ));
+		System.out.println(pairs(arr, arr.length, k));
 
 	}
 
 	private static int pairs(int[] arr, int length, int k) {
-		int left=0, right=arr.length-1, sum=0;
-		while(left<right) {
-			sum=arr[left]+arr[right];
-			
-			if(sum==k) {
+		int left = 0, right = arr.length - 1, sum = 0;
+		while (left < right) {
+			sum = arr[left] + arr[right];
+
+			if (sum == k) {
 				return 1;
-			}else if(sum<k) {
+			} else if (sum < k) {
 				left++;
-			}else {
+			} else {
 				right--;
 			}
-			
+
 		}
-		
-		return -1;	
+
+		return -1;
 	}
 
 }
